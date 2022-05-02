563e7cd39d7996e1e274f46b43f870ce0dbc15e6
DarkLorian <66025673+DarkLorianPrime@users.noreply.github.com>
2022-05-02 19:24:07 +0400

    Update Bot_Longpoll.py

--- examples/Bot_Longpoll.py
+++ examples/Bot_Longpoll.py
@@ -1,14 +1,11 @@
 import os
 
-from vk_dark_library.VKBotLongPoll import VKLongPoll, NEW, user_or_chat, load_config, get_api
-from vk_dark_library.VKCommandHandler import CommandHandler
+from vk_dark_library.VKBotLongPoll import VKLongPoll, NEW, GetApi, LoadConfig
 
-load_config(os.getcwd(), "config")
-vk = get_api()
+LoadConfig(os.getcwd(), "config")
+vk = GetApi()
 
 for vk_object in VKLongPoll().listen():
     if vk_object.type == NEW:
-        CommandHandler().check_updates(vk_object)
         if vk_object.text == "hello":
-            send_id = user_or_chat(vk_object)
-            vk.messages.send(**send_id, message="И тебе привет, путник!", random_id=0)
+            vk.messages.send(**vk_object.send_id, message="И тебе привет, путник!", random_id=0)
