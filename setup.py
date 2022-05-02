df7bb6a885f6066e8bfa53a2ac0307e63f05f75b
darklorianprime <lolpokens@mail.ru>
2022-05-02 19:46:54 +0400

    Add debug system and remake functionality.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(name='vk_dark_library',
-      version='0.1.2',
-      url='https://github.com/the-gigi/pathology',
+      version='1.0.5',
+      url='https://github.com/DarkLorianPrime/vk_library',
       license='Apache License, Version 2.0, see LICENSE file',
       author='Darklorian (Aleksander Kasimov)',
       author_email='lolpokens@mail.ru',
-      description='Быстрое и удобное создание ботов в вк. Как LongPoll так и Callback',
+      description='Быстрое и удобное создание ботов в вк. Пока только LongPoll.',
       packages=find_packages(),
       long_description=readme,
       install_requires=["requests>=2"])
