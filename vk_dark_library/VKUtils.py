c0fa86fdda4dc06a4e3193e721f787671aa83581
DarkLorian <66025673+DarkLorianPrime@users.noreply.github.com>
2022-05-02 18:29:34 +0400

    Add files via upload

---
+++ vk_dark_library/VKUtils.py
@@ -0,0 +1,15 @@
+class VKcolors:
+    HEAD = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARN = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+DEBBUGER_VERSION = "1.3.1"
+LONGPOLL_VERSION = "2.0.1"
+CALLBACK_VERSION = "0.0.0"
